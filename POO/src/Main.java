bae9e2dbef93f56bc5e81a03659f13154db92a7c
camilaavillaa <ana_camila_@live.com.mx>
2023-08-22 09:44:46 -0600

    Tarea 1 Programa de una calculadora que solo realiza 5 procesos, suma, resta, multiplicacion y division

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,5 +1,7 @@
+import edu.camilaavillaa.reto1.ui.CLI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CLI.showMenu();
     }
 }
