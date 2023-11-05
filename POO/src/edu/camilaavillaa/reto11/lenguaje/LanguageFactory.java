72e263ec367f49798a2e10735697ecd6876c41e5
camilaavillaa <ana_camila_@live.com.mx>
2023-11-04 23:31:21 -0600

    En este reto 11 realizo el procedimiento de un programa para hacer una calculadora aritmetica en donde podemos sacar suma, resta, division, etc

---
+++ POO/src/edu/camilaavillaa/reto11/lenguaje/LanguageFactory.java
@@ -0,0 +1,34 @@
+package edu.camilaavillaa.reto11.lenguaje;
+
+public class LanguageFactory {
+    private static Lenguaje language;
+
+    public static void setLanguage(int languageOption) {
+        switch (languageOption) {
+            case 1:
+                language = new Español();
+                break;
+            case 2:
+                language = new English();
+                break;
+            case 3:
+                language = new Japanese();
+                break;
+            case 4:
+                language = new Russian();
+                break;
+
+            default:
+                System.out.println("Idioma no soportado. Seleccionando español por defecto.");
+                language = new Español();
+        }
+    }
+
+    public static String getMessage(String key) {
+        if (language != null) {
+            return language.getMessage(key);
+        }
+        return "Idioma no configurado";
+    }
+}
+
